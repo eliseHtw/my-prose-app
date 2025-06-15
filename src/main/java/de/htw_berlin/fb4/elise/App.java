6691c125cb9dae18ad9046979583f03db4040bb3
elise <elise.schwentesius@student.htw-berlin.de>
2025-06-15 16:09:19 +0200

    package created, java doc added, git tagged

--- src/main/java/de/htw_berlin/fb4/elise/App.java
+++ src/main/java/de/htw_berlin/fb4/elise/App.java
@@ -7,17 +7,20 @@ import de.htw_berlin.fb4.ossd.prose.Sentence;
 import java.util.List;
 
 /**
- * Hello world!
- *
+ * Application entry point that demonstrates the usage of ProseBuilder.
  */
 public class App
 {
+    /**
+     * The main method that runs the demonstration.
+     *
+     * @param args the command line arguments
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
 
         Sentence s1 = new SimpleSentence();
-
         ProseBuilder proseBuilder = new ProseBuilder();
         proseBuilder.register(s1);
         System.out.println(proseBuilder.get());
--- src/main/java/de/htw_berlin/fb4/elise/SimpleSentence.java
+++ src/main/java/de/htw_berlin/fb4/elise/SimpleSentence.java
@@ -2,14 +2,15 @@ package de.htw_berlin.fb4.elise;
 
 import de.htw_berlin.fb4.ossd.prose.Sentence;
 
+/**
+ * Represents a simple sentence with a fixed text content.
+ */
 public class SimpleSentence implements Sentence {
-
-    // private String sentence = "Ein Beispielsatz";
-
-    /* public SimpleSentence(String sentence) {
-        this.sentence = sentence;
-    } */
-
+    /**
+     * Returns the fixed sentence content.
+     *
+     * @return the fixed sentence "Ich esse heute Abend Salat"
+     */
     @Override
     public String get() {
         return "Ich esse heute Abend Salat";
